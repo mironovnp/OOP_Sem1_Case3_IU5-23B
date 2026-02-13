0c363e8272fdb2a5b8f39d92a4a0868118486ef3
Avannaki <korotenko.2007@mail.ru>
2026-02-13 13:06:48 +0300

    Made Student.h

---
+++ include/Student.h
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Student{
+    //Статическое поле с счётчиком id всех студентов(для уникальности)
+    static int nextIdCount;
+
+    //Поля 
+    const int studentId;
+    string name;
+    vector<int> grades;
+
+    public:
+        //Конструкторы 
+        Student();
+        Student(const string& name, const vector<int>& initGrades);
+
+        //Методы класса
+        void addGrade(int grade);
+        double calculateAverage() const;
+        void printInfo() const;
+        
+        //Геттер
+        int getID() const {return studentId;}
+};
