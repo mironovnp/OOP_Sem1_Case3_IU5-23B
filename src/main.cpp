90ab28a13ec6fa862aee740fb3c8ca4f5716d5ae
dyatilpn <dictor.net@gmail.com>
2026-02-15 22:57:36 +0300

    Добавлена локализация тестирования на русский язык. Сборка успешна. Ошибок и предупреждений нет. Работа корректна. 15/02/2026 (22:58)

--- src/main.cpp
+++ src/main.cpp
@@ -23,11 +23,11 @@ int main() {
 
     //Создаем объекты студентов.
 
-    Student s1("Nikita Mironov", {});
-    Student s2("Andrey Korotenko", {});
-    Student s3("Alexander Fedyaev", {});
-    Student s4("Ivan Nemtsov", {});
-    Student s5("Gayana Akhmetzyanova", {});
+    Student s1("Никита Миронов", {});
+    Student s2("Андрей Коротенко", {});
+    Student s3("Александр Федяев", {});
+    Student s4("Иван Немцов", {});
+    Student s5("Гаяна Ахметзянова", {});
 
     //Добавляем оценки
     s1.addGrade(5); s1.addGrade(4); s1.addGrade(5);
@@ -36,15 +36,15 @@ int main() {
     s4.addGrade(3); s4.addGrade(4); s4.addGrade(3);
     s5.addGrade(5); s5.addGrade(5); s5.addGrade(5);
 
-    std::cout << "\nAdding students to group...\n";
+    std::cout << "\nДобавление студентов в группу...\n";
     group.addStudent(s1);
     group.addStudent(s2);
     group.addStudent(s3);
     group.addStudent(s4);
     group.addStudent(s5);
-    std::cout << "Total students added.\n\n";
+    std::cout << "Все студенты успешно добавлены в группу.\n\n";
 
-    std::cout << "Individual student reports:\n";
+    std::cout << "Индивидуальная информация о студентах:\n";
     std::cout << "------------------------------------\n";
     s1.printInfo();
     s2.printInfo();
@@ -53,19 +53,19 @@ int main() {
     s5.printInfo();
 
     // Тестируем поиск студента по ID (например, 4)
-    std::cout << "\nSearching for student with ID (4)...\n";
+    std::cout << "\nПоиск студента с ID(4)...\n";
     Student found = group.findStud(4);
     if (found.getId() != 0) { // Проверка, если ID найден
         found.printInfo();
     }
 
-    std::cout << "\nGroup Statistics:\n";
+    std::cout << "\nСтатистика группы:\n";
     std::cout << "------------------------------------\n";
     double avg = group.calcAv();
-    std::cout << "Overall Group Average Grade: " << avg << "\n\n";
+    std::cout << "Средняя оценка по всей группе: " << avg << "\n\n";
 
     std::cout << "========================================\n";
-    std::cout << "          TESTING COMPLETED\n";
+    std::cout << "          ЗАВЕРШЕНИЕ ТЕСТИРОВАНИЯ\n";
     std::cout << "========================================\n";
 
     return 0;
